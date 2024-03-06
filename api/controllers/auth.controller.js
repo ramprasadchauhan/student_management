dd12556aabac18787a95be0427168dccd7e90832
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-03-06 20:12:21 +0530

    complete signin page functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -25,6 +26,39 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.json("Signup successful");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All field are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(403, "User not found"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+    const token = jwt.sign(
+      {
+        id: validUser._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: 7 * 24 * 60 * 60 * 1000 }
+    );
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_cookie", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -8,9 +8,9 @@ const SignUp = () => {
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.id]: e.target.value });
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
-
+  // console.log(formData);
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.username || !formData.email || !formData.password) {
@@ -25,6 +25,7 @@ const SignUp = () => {
         body: JSON.stringify(formData),
       });
       const data = await res.json();
+      console.log(data);
       if (data.success === false) {
         return setErrorMessage(data.message);
       }
@@ -37,17 +38,14 @@ const SignUp = () => {
       setLoading(false);
     }
   };
-
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left */}
         <div className="flex-1">
-          <Link
-            to="/"
-            className=" text-4xl sm:text-xl font-bold sdark:text-white"
-          >
+          <Link className="font-bold dark:text-white text-4xl">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
-              Students
+              Media
             </span>
             Blog
           </Link>
@@ -56,6 +54,7 @@ const SignUp = () => {
             or with google
           </p>
         </div>
+        {/* right */}
         <div className="flex-1">
           <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div>
@@ -68,16 +67,16 @@ const SignUp = () => {
               />
             </div>
             <div>
-              <Label value="Email" />
+              <Label value="Your email" />
               <TextInput
                 type="email"
-                placeholder="username@gmail.com"
+                placeholder="name@company.com"
                 id="email"
                 onChange={handleChange}
               />
             </div>
             <div>
-              <Label value="Password" />
+              <Label value="Your Password" />
               <TextInput
                 type="password"
                 placeholder="Password"
@@ -90,25 +89,25 @@ const SignUp = () => {
               type="submit"
               disabled={loading}
             >
-              SignUp
-            </Button>
-          </form>
-          <div className="flex text-sm mt-5 gap-2">
-            <span>Have an account?</span>
-            <Link to="/sign-in" className="text-blue-500">
               {loading ? (
                 <>
                   <Spinner size="sm" />
-                  <span>Loading...</span>
+                  <span className="pl-3">Loading...</span>
                 </>
               ) : (
                 "Sign Up"
               )}
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Have an account</span>
+            <Link to="/sign-in" className="text-blue-500 hover:text-pink-600">
+              Sign In
             </Link>
           </div>
           {errorMessage && (
             <Alert className="mt-5" color="failure">
-              {errorMessage}{" "}
+              {errorMessage}
             </Alert>
           )}
         </div>
