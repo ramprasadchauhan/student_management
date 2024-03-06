8c1dc658f5a2e89dfc1029bd155a68512e56e132
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-03-06 11:37:54 +0530

    initialize add with client and api folder and  make responsive header section

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import Dashboard from "./pages/Dashboard";
+import Student from "./pages/Student";
+import Header from "./components/Header";
+import About from "./pages/About";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/student" element={<Student />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
