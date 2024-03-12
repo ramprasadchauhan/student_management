c6c6ce0e1c60426fbb79bd79204557c0a493da0d
Ramprasad Chauhan <ramprasadchauhan7@gmail.com>
2024-03-13 00:19:12 +0530

    fix index

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/student", studentRoute);
 
-app.use(express.static(path.resolve.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
